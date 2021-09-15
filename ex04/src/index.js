5bdfb0dfc029d0dfa82a0e7d2d834701fc4f32b6
unknown <sanninm>
2021-09-15 22:24:26 +0200

    Variables

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+function main (){
+    // Only change code below this line 
+    
+    
+    var x = 10;
+    var y = 10;
+    var x = y;
+    
+    // Only change code aboce this line
+    return [x, y];
+    
+    }
+    
+    console.log(main());
+    
+    module.exports = main;
