5f7438c350cc0b49f1fd4047e09ca498536e1cd6
unknown <sanninm>
2021-09-19 08:53:48 +0200

    Case Sensitivity in Variables

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -2,11 +2,16 @@ function main (){
     // Only change code below this line 
     // Variable declarations
 
-    var studlyCapVar = 10;
-    var properCamelCase = "A String";
-    var titleCaseOver = 9000;
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
 
     // Variable assignments
+
+    studlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver = 9000;
+
     // Only change code aboce this line
     
 
